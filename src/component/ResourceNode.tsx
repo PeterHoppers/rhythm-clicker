5950c0bcf5372665c44d5753b6ea4714e8d2276c
Peter Hoppe <peterhoppe341@gmail.com>
2025-03-12 11:10:57 -0500

    Can Press A Button Through Keyboard
    - instead of having to click on the beat, one can also press buttons on the beat, since moving the mouse between buttons is hard to do

--- src/component/ResourceNode.tsx
+++ src/component/ResourceNode.tsx
@@ -1,18 +1,62 @@
 import { ResourceData } from "../lib/definitions";
 import styles from "./resourceNode.module.css";
+import { useEffect, useState } from "react";
 
 //this takes the current information about the currency and displays the basic information, along with a button that can trigger the gain money call back
 
 interface ResourceNodeProps {
     resourceData: ResourceData,
+    keyCode: string,
     onClickCallback: () => void
 }
 
 export default function ResourceNode(props: ResourceNodeProps) {
+    const [isPressed, setPressed] = useState<boolean>(false);
     const info = props.resourceData.resource.resourceInfo;
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDownEvent);
+        return window.addEventListener('keydown', handleKeyDownEvent);
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('keyup', handleKeyUpEvent);
+        return window.addEventListener('keyup', handleKeyUpEvent);
+    }, [])
+
+    const handleKeyDownEvent = (event: KeyboardEvent) => {
+        if (event.key === props.keyCode && !event.repeat) {
+            onPressedButton();
+        }
+    };
+
+    const handleKeyUpEvent = (event: KeyboardEvent) => {
+        if (event.key === props.keyCode) {
+            onReleaseButton();
+        }
+    }
+
+    const onPressedButton = () => {
+        setPressed((isPressed) => {
+            if (isPressed) {
+                return isPressed;
+            }
+
+            props.onClickCallback();
+            return true;
+        });
+        
+    }
+
+    const onReleaseButton = () => {
+        setPressed(false);
+    }
+
+    const classNames = `${styles.button} ${(isPressed) ? styles.pressed : styles.unpressed}`;
+
     return (
         <>
-            <button className={styles.button} onClick={props.onClickCallback}>
+            <button className={classNames} onMouseDown={onPressedButton} onMouseLeave={onReleaseButton} onMouseUp={onReleaseButton} onTouchStart={onPressedButton} onTouchEnd={onReleaseButton} onTouchCancel={onReleaseButton}>
                 {info.displayIcon}
             </button>         
         </>
